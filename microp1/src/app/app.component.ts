c9c6735b30660c02859cef70a34186436a740336
s0f1a <100363545@alumnos.uc3m.es>
2019-02-07 21:13:05 +0100

    Microp1
    Micropráctica 1

---
+++ microp1/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+title = 'Micro-práctica 1' ;
+private productos : Array <[ string , number ]>=[[ 'Arroz tres delicias' , 3.50 ],[ 'Cerdoagridulce' , 5.50 ],[ 'Ternera con bambu y setas' , 7.00 ]];
+public codProducto =- 1 ;
+public importe : number = 0.0 ;
+public lineas: Array <[ string , number ]>= [] ;
+public addLinea ()
+{
+var producto = this . productos [ this . codProducto ][ 0 ];
+var precio : number = this . productos [ this . codProducto ][ 1 ];
+this . lineas . push ([ producto , precio ]);
+this . importe = this . importe + precio ;
+}
+public delLinea ( i )
+{
+this . importe -= this . lineas [ i ][ 1 ];
+this . lineas . splice ( i , 1 );
+}
+}
